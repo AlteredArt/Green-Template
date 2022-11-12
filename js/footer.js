7b1f1fe41cec437727f2e17fbf802c80d4234d4f
Jared Matta <cherokeedreemer@gmail.com>
2022-11-12 08:33:28 -0700

    inside

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ class Footer extends HTMLElement{
         <a onclick="topFunction()" id="backToTop" title="Go2top"><i class="fa fa-arrow-circle-up"></i></a>
         
         <footer>
-          <div id="footer" class="">
+          <div id="footer" class="container">
     
             <div class='flex col jcspace aicenter'>
     
@@ -19,12 +19,10 @@ class Footer extends HTMLElement{
                 <p class='footer-social-item'><a href="https://www.messenger.com/login.php?next=https%3A%2F%2Fwww.messenger.com%2Ft%2F100992561910609%2F%3Fmessaging_source%3Dsource%253Apages%253Amessage_shortlink%26source_id%3D1441792%26recurring_notification%3D0"><i class="fa-brands fa-facebook-messenger tooltip"><span class="tooltiptext">FBMessenger</span></i></a></p>
                 <p class='footer-social-item'><a href="https://square.site/book/L98NPR7TF8NX2/knight-vision-inspection"><i class="fa-solid fa-calendar-check tooltip"> <span class="tooltiptext">Book Now</span></i></a></p>
               </div>
-    
-              <div class="sub-footer ta"><h4>Knight Vision Inspection <span>&#169;</span> 2022</h4></div>
-              
             </div>
-    
           </div>
+          <div class="sub-footer ta"><h4>Knight Vision Inspection <span>&#169;</span> 2022</h4></div>
+
         </footer>
     
         `
